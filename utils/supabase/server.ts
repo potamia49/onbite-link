88e16a0cad333cb4c0bfb5021c66ba457c9d1a1b
윤일근 <potamia49@gmail.com>
2026-06-21 10:31:32 +0900

    슈퍼베이스 연동 설정

---
+++ utils/supabase/server.ts
@@ -0,0 +1,25 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
+  return createServerClient(supabaseUrl!, supabaseKey!, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
+      },
+      setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options),
+          );
+        } catch {
+          // Server Component에서 호출된 경우 무시합니다.
+          // proxy에서 세션을 갱신하고 있다면 안전합니다.
+        }
+      },
+    },
+  });
+};
